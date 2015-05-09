31a5421415dd66e574d57458592e0e48acff2dda
Nathan R <nathan19@hotmail.com>
2015-05-09 10:53:11 -0700

    edit and destroy notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :find_note, only: [:show, :edit, :destroy] 
+  before_action :find_note, only: [:show, :edit, :update, :destroy] 
 
   def index 
     @notes = Note.all.order("created_at DESC")
@@ -24,10 +24,18 @@ class NotesController < ApplicationController
   def edit
   end
 
-  def update 
+  def update
+    if @note.update(note_params)
+      redirect_to @note
+    else 
+      render 'edit' 
+    end 
   end
 
   def destroy
+    @note.destroy
+    redirect_to @note 
+
   end
 
 private
